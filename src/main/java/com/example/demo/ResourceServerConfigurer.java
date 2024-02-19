578cc972acfa97d55320c117c1a6a9bf85668d71
ArthurTogni <atognisilva@gmail.com>
2024-02-18 21:22:54 -0300

    config security

--- src/main/java/com/example/demo/ResourceServerConfigurer.java
+++ src/main/java/com/example/demo/ResourceServerConfigurer.java
@@ -1,19 +1,18 @@
-//package com.example.demo;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-//
-//@Configuration
-//public class ResourceServerConfigurer extends ResourceServerConfigurerAdapter{
-//
-//    @Override
-//    public void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers(HttpMethod.POST, "/api")
-//                .hasRole("ADMIN")
-//        ;
-//    }
-//
-//}
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+public class ResourceServerConfigurer extends ResourceServerConfigurerAdapter{
+
+    @Override
+    public   void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/**").authenticated() // Barrar chamadas que começam com "/api"
+                .anyRequest().permitAll(); // Permitir todas as outras chamadas
+
+    }
+
+}
